ce8c727237b506af5bcbd3b99a7d95ffe977fe2a
Poulad <poulad.pld@gmail.com>
2017-07-11 12:34:46 -0400

    Add unit test proj

---
+++ test/NextBus.NET.Tests/AgencyTests.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NextBus.NET.Tests
+{
+    public class AgencyTests
+    {
+        [Fact(DisplayName = "Should get all available agencies")]
+        public async Task ShouldGetAgencies()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
